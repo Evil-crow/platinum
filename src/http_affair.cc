c508e57858bc329710c3d0c61e94fb84850e9c8b
Evil-crow <Evilcrow486@gmail.com>
2018-08-24 02:50:53 +0800

    only create the file

---
+++ src/http_affair.cc
@@ -0,0 +1,9 @@
+/*
+ * Created by Crow on 8/24/18.
+ * Description: This file is so important in PlatinumServer, It's used to deal with the HTTP affair
+ *          It bases on <httpxx/Request.hpp>, <httpxx/ResponseBuilder.hpp> and "http_response.h"
+ *          to deal with the HTTP-affair, by parse the recv-data and build-response
+ *  API:
+ *
+ */
+
