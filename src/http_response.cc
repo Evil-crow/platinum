13b2fa8a7401ab15bca002dd488a6e8377cdefb1
Evil-crow <Evilcrow486@gmail.com>
2018-08-24 23:51:19 +0800

    repair a BUG: can't set the headers

--- src/http_response.cc
+++ src/http_response.cc
@@ -6,15 +6,14 @@
 #include <string>
 #include "http_response.h"
 
-bool PlatinumServer::http_response::set_header(std::string &key, std::string &val)
+bool PlatinumServer::http_response::set_header(std::string &&key, std::string &&val)
 {
-    auto http_headers = headers();                      // get the headers
-    http_headers.insert(std::make_pair(std::move(key), std::move(val)));
+    this->headers().insert(std::make_pair(std::move(key), std::move(val)));
 
     return true;
 }
 
-bool PlatinumServer::http_response::set_body(std::string &body)
+bool PlatinumServer::http_response::set_body(std::string &&body)
 {
     this->myResponseBody = std::move(body);
 
@@ -33,7 +32,7 @@ bool PlatinumServer::http_response::build()
     auto http_body = this->get_body();
 
     // build the http-response
-    this->server_response = http_status_line + "\r\n";
+    this->server_response = http_status_line + "\r\n";        // The httpxx library has add "\r\n" (modify the code);
     for (auto &i : http_headers)
         this->server_response += i.first + ": " + i.second + "\r\n";
     this->server_response += "\r\n";
@@ -43,6 +42,7 @@ bool PlatinumServer::http_response::build()
 
     return true;
 }
+
 const std::string &PlatinumServer::http_response::get_response()
 {
     return this->server_response;
