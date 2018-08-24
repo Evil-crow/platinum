98e2d2399dcbaad4adbbe03235c644cb181517db
Evil-crow <Evilcrow486@gmail.com>
2018-08-24 23:41:32 +0800

    the main peocess about WebServer

---
+++ include/server.h
@@ -0,0 +1,19 @@
+/*
+ * Created by Crow on 8/24/18.
+ * Description: Function declaration for server;
+ * Function:
+ *          void epoll_server(in_port_t port);                      // from the main() to server process
+ *          void epoll_deal(PlatinumServer::epoll &epollobj);      // deal with the I/O event
+ */
+
+#ifndef PLATINUMSERVER_SERVER_H
+#define PLATINUMSERVER_SERVER_H
+
+#include <arpa/inet.h>
+#include "epoll.h"
+
+void epoll_server(in_port_t port);
+
+void epoll_deal(int listen_fd, PlatinumServer::epoll &epollobj, struct epoll_event *evlist);
+
+#endif //PLATINUMSERVER_SERVER_H
