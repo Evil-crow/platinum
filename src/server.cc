eb85b5e0a040dbb81eab57522ecbaaa3c428eb59
Evil-crow <Evilcrow486@gmail.com>
2018-08-26 14:03:33 +0800

    use the LT, not ET

--- src/server.cc
+++ src/server.cc
@@ -6,8 +6,6 @@
  */
 
 #include <arpa/inet.h>
-#include <array>
-#include <unistd.h>
 #include "thread_pool.h"
 #include "http_affair.h"
 #include "server.h"
@@ -16,62 +14,40 @@
            do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 const int MAX_EPOLL_LIST = 1000;
+PlatinumServer::thread_pool pool(8);
 
 void epoll_server(in_port_t port)
 {
-    PlatinumServer::socket listen_socket(port);       // create socket object
-    listen_socket.set_non_blocking();              // set O_NONBLOCK for epoll()[ET],
-    listen_socket.connect();                       // finish the bind() and listen()
-
     struct epoll_event evlist[MAX_EPOLL_LIST];
     PlatinumServer::epoll epoll;
-    epoll.add(listen_socket.get_fd(), { EPOLLIN, EPOLLET });                     // add listen fd to epoll event table
+    PlatinumServer::socket listen_socket(port);                                                 // create socket object
+    listen_socket.connect();                                                                    // finish the bind() and listen()
 
+    epoll.add(listen_socket.get_fd(), { EPOLLIN });                                           // add listen fd to epoll event table
     while (true) {
         epoll.wait(evlist, MAX_EPOLL_LIST, -1);
         switch (epoll.get_io_count()) {
             case -1:                                                                          // epoll_wait() error
                 ERR_HANDLE("epoll_wait");
                 break;
-            case 0:                                                                           // no-event
-                break;
             default:                                                                          // IO event
-                epoll_deal(listen_socket.get_fd(), epoll
-                           , evlist);                                    // deal with the epoll I/O event
+                epoll_deal(listen_socket, epoll, evlist);                                     // deal with the epoll I/O event
                 break;
         }
     }
 }
 
-void epoll_deal(int listen_fd, PlatinumServer::epoll &epollobj, struct epoll_event *evlist)
+void epoll_deal(PlatinumServer::socket &listen_socket, PlatinumServer::epoll &epollobj, struct epoll_event *evlist)
 {
-    PlatinumServer::socket listen_socket(listen_fd);
-
     for (int i = 0; i < epollobj.get_io_count(); ++i) {
-        if ((evlist[i].data.fd == listen_socket.get_fd())
-                && (evlist[i].events & EPOLLIN)) {             // I/O event happen on listen_fd;
-            int conn_fd;
-
-            // while(accept()) for epoll()[ET];
-            while ((conn_fd = listen_socket.accept()) > -1) {
-                std::cout << "conn_fd: " << conn_fd << "errno: " << errno << std::endl;
-                PlatinumServer::socket conn_socket(conn_fd);                // Construct the socket by conn_fd
-                conn_socket.set_non_blocking();                             // set NON-BLOCK
-                epollobj.add(conn_socket.get_fd(), { EPOLLIN, EPOLLET, EPOLLONESHOT });         // add event
-            }
-        }
-        else if ((evlist[i].events & EPOLLRDHUP)
-                || (evlist[i].events & EPOLLERR)) {            // the client has hang up
-            std::cout << "client hang out" << std::endl;
-            epollobj.del(evlist[i].data.fd);
-            ::close(evlist[i].data.fd);
-        }
-        else {
-            if ((evlist[i].events & EPOLLIN)
-                    && !(evlist[i].events & EPOLLRDHUP)) {            // general I/O event, the HTTP affair
-                std::cout << "got in the pool" << std::endl;
-                deal_http_affair(PlatinumServer::socket(evlist[i].data.fd), epollobj);
-            }
+        if ((evlist[i].data.fd == listen_socket.get_fd()) && (evlist[i].events & EPOLLIN)) {                                         // I/O event happen on listen_fd;
+            int conn_fd = listen_socket.accept();
+            epollobj.add(conn_fd, { EPOLLIN, EPOLLONESHOT });                           // add event
+        } else if ((evlist[i].events & EPOLLIN)) {                                      // general I/O event, the HTTP affair
+            PlatinumServer::socket conn_socket(evlist[i].data.fd);
+            deal_http_affair(conn_socket, epollobj);
+            // auto task = [&](){ return func(conn_socket, epollobj); };
+            // pool.submit_task(task);
         }
     }
 }
