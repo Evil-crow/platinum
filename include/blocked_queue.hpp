6800f9fbf1afc8d1c63ad5caae4db776c33a1b3c
Evil-crow <Evilcrow486@gmail.com>
2018-08-26 13:59:22 +0800

    modify the format

--- include/blocked_queue.hpp
+++ include/blocked_queue.hpp
@@ -15,56 +15,56 @@
 #include <condition_variable>
 
 namespace PlatinumServer {
-  template <typename T>
-  class blocked_queue {
-  public:
-      blocked_queue() = default;
-      bool push(T &&data);
-      bool pop(T &val);
-  private:
-      std::mutex mtx;                        // single-lock to block between push() & pop()
-      std::condition_variable cv;
-      std::queue<T> queue;
+template <typename T>
+class blocked_queue {
+public:
+    blocked_queue() = default;
+    bool push(T &&data);
+    bool pop(T &val);
+private:
+    std::mutex mtx;                        // single-lock to block between push() & pop()
+    std::condition_variable cv;
+    std::queue<T> queue;
 
-      bool empty();
-  };
+    bool empty();
+};
 
-  template <typename T>
-  bool blocked_queue<T>::push(T &&data)
-  {
-      std::lock_guard<std::mutex> lock(mtx);                      // blocked the pop();
-      queue.emplace(std::move(data));
-      cv.notify_one();
+template <typename T>
+bool blocked_queue<T>::push(T &&data)
+{
+    std::lock_guard<std::mutex> lock(mtx);                      // blocked the pop();
+    queue.emplace(std::move(data));
+    cv.notify_one();
 
-      return true;
-  }
+    return true;
+}
 
-  template <typename T>
-  bool blocked_queue<T>::pop(T &val)
-  {
-      std::unique_lock<std::mutex> lock(mtx);                     // lock_guard isn't suitable for wait();
+template <typename T>
+bool blocked_queue<T>::pop(T &val)
+{
+    std::unique_lock<std::mutex> lock(mtx);                     // lock_guard isn't suitable for wait();
 
-      /*
-       * the same as:
-       * while (empty()) {
-       *      cv.wait(lock);
-       * }
-      */
-      cv.wait_for(lock, std::chrono::microseconds(100), [&]() { return !empty(); });
-      if (empty()) {
-          return false;
-      }
-      else {
-          val = queue.front();
-          queue.pop();
-          lock.unlock();
-          return true;
-      }
-  }
+    /*
+     * the same as:
+     * while (empty()) {
+     *      cv.wait(lock);
+     * }
+    */
+    cv.wait_for(lock, std::chrono::microseconds(100), [&](){ return !empty(); });
+    if (empty()) {
+        return false;
+    }
+    else {
+        val = queue.front();
+        queue.pop();
+        lock.unlock();
+        return true;
+    }
+}
 
-  template <typename T>
-  bool blocked_queue<T>::empty()
-  {
-      return queue.empty();                       // only use empty() in pop(), have gotten the mutex;
-  }
+template <typename T>
+bool blocked_queue<T>::empty()
+{
+    return queue.empty();                       // only use empty() in pop(), have gotten the mutex;
+}
 }
