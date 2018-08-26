3085b663c0f31ebd0a5418574d20cb0d41187918
Evil-crow <Evilcrow486@gmail.com>
2018-08-26 14:03:06 +0800

    ,

--- src/main.cc
+++ src/main.cc
@@ -1,42 +1,9 @@
 #include <iostream>
-#include <thread>
-#include <unistd.h>
+#include "server.h"
 #include "thread_pool.h"
-
-void foo()
-{
-    std::cout << "this is foo" << std::endl;
-}
-
-void too()
-{
-    std::cout << "this is too" << std::endl;
-}
-
-void func()
-{
-    std::cout << "this is func" << std::endl;
-}
-
-void fuck()
-{
-    std::cout << "this is fuck" << std::endl;
-}
 
 int main(int argc, char *argv[])
 {
-    PlatinumServer::thread_pool pool(2);
-
-    auto i = [](){ return foo(); };
-    auto j = [](){ return too(); };
-    auto k = [](){ return func(); };
-    auto l = [](){ return fuck(); };
-
-    pool.submit_task({i});
-    pool.submit_task({j});
-    pool.submit_task({k});
-    pool.submit_task({l});
-    usleep(100);
-
+    epoll_server(8090);
     return 0;
 }
