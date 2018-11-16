fb168504258793bde832c9130b45682e2bbda238
Evil-crow <Evilcrow486@gmail.com>
2018-11-17 01:47:55 +0800

    Class LoggerOn, LoggerOff, Logger

---
+++ utility/log.h
@@ -0,0 +1,63 @@
+//
+// Created by Crow on 11/15/18.
+//
+
+#ifndef PLATINUM_LOG_H
+#define PLATINUM_LOG_H
+
+#include <string>
+#include <vector>
+
+#include "../config/config.h"
+
+namespace Platinum {
+
+#define ERROR "ERROR"
+#define WARN "WARN"
+#define INFO "INFO"
+#define LOG(PRIORITY) (Logger::GetLog()).SetLevel(PRIORITY); Logger::GetLog()
+
+class Logger {
+ public:
+  Logger(const Logger &) = delete;
+  void operator=(const Logger &) = delete;
+
+  static Logger &GetLog();
+  virtual void operator<<(const std::string &&vec) = 0;
+  virtual void SetLevel(const std::string &&priority) = 0;
+
+ protected:
+  Logger() = default;
+  virtual ~Logger() = default;
+
+};
+
+class LoggerOn : public Logger {
+ public:
+  static Logger &GetInstance();
+  void operator<<(const std::string &&vec) override;
+  void SetLevel(const std::string &&priority) override;
+
+ private:
+  LoggerOn() = default;
+  ~LoggerOn() override = default;
+
+  std::string priority_;
+};
+
+class LoggerOff : public Logger {
+ public:
+  static Logger &GetInstance();
+  void operator<<(const std::string &&vec) override {}  // Class LoggerOff is defined to do nothing ! (For Logger is off)
+  void SetLevel(const std::string &&priority) override {}
+
+ private:
+  LoggerOff() = default;
+  ~LoggerOff() override = default;
+
+  // Class LoggerOff can not provide the private member data --priority
+};
+
+}
+
+#endif //PLATINUM_LOG_H
