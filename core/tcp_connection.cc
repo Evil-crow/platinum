7341a30ce245e40f467e3a48d17eadf858b72746
Evil-crow <Evilcrow486@gmail.com>
2018-11-23 23:36:08 +0800

    ...

---
+++ core/tcp_connection.cc
@@ -0,0 +1,5 @@
+//
+// Created by Crow on 11/23/18.
+//
+
+#include "tcp_connection.h"
---
+++ core/tcp_connection.h
@@ -0,0 +1,23 @@
+//
+// Created by Crow on 11/23/18.
+//
+
+#ifndef PLATINUM_TCP_CONNECTION_H
+#define PLATINUM_TCP_CONNECTION_H
+
+namespace platinum {
+
+class TCPListenSocket;
+class TCPConnection {
+  friend class TCPListenSocket;
+ public:
+
+ private:
+  TCPConnection();
+
+
+};
+
+}
+
+#endif //PLATINUM_TCP_CONNECTION_H
