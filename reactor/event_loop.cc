11b18ccdcabb1775b03ae179f5d2ae79ecebdb64
Evil-crow <Evilcrow486@gmail.com>
2018-12-03 23:27:17 +0800

    add EventLoop::loop, active_hannel_

--- reactor/event_loop.cc
+++ reactor/event_loop.cc
@@ -1,29 +1,64 @@
-//
 // Created by Crow on 11/25/18.
 //
 
 #include "event_loop.h"
 
+#include <unistd.h>
+#include <sys/eventfd.h>
 #include <thread>
 #include <utility/logger.h>
 
 using namespace platinum;
 
-thread_local EventLoop *t_event_loop = nullptr;
+thread_local EventLoop *t_event_loop = nullptr;            // thread_local, to indicate there is only one eventloop in the thread.
 
 EventLoop::EventLoop()
-    : looping_(false), thread_id_(std::this_thread::get_id())
+    : looping_(false),
+      wakeup_fd_(::eventfd(0, 0)),
+      wakeup_channel_(new Channel(this, wakeup_fd_)),
+      thread_id_(std::this_thread::get_id())
 {
-  t_event_loop = this;
+  if (t_event_loop) {
+    LOG(ERROR) << "EventLoop has been created";
+    AbortInLoopThread();
+  } else {
+    t_event_loop = this;
+  }
 }
 
 EventLoop::~EventLoop()
 {
-  if (!looping_)
+  if (!looping_.load())
     t_event_loop = nullptr;
 }
 
-void EventLoop::loop() {}
+void EventLoop::Loop()
+{
+  while (!quit_.load()) {
+
+  }
+}
+
+void EventLoop::Quit()
+{
+  quit_.store(false);
+  if (!looping_.load())
+    WakeUp();
+}
+
+void EventLoop::WakeUp()
+{
+  uint64_t u;
+  ::write(wakeup_fd_, &u, sizeof(u));
+  LOG(INFO) << "Wake Up Event";
+}
+
+void EventLoop::HandleWakeUp()
+{
+  uint64_t u;
+  ::read(wakeup_fd_, &u, sizeof(u));
+  LOG(INFO) << "Handle Wake Event";
+}
 
 bool EventLoop::AssertInLoopThread()
 {
--- reactor/event_loop.h
+++ reactor/event_loop.h
@@ -5,25 +5,53 @@
 #ifndef PLATINUM_EVENTLOOP_H
 #define PLATINUM_EVENTLOOP_H
 
+#include <atomic>
 #include <thread>
+#include <functional>
+#include <vector>
+#include <mutex>
 
 namespace platinum {
 
+class Channel;
+class EPoller;
 class EventLoop {
  public:
+  // All Functor or Callback, use this std::function format.
+  // You can use std::bind, lambda expression ( I suggest you use lambda expression !)
+  using Functor = std::function<void ()>;
+
   EventLoop();
   ~EventLoop();
 
-  void loop();
+  void Loop();                                 // The most important interface of EventLoop, to loop !
+  void RunInLoop(Functor cb);                  // One of the other important interface, allow other thread move their task into this IO thread
 
-  bool AssertInLoopThread();
+  void Quit();                                 // if want to quit loop, use this interface(you may not need this indeed
+
+  bool AssertInLoopThread();                   // judge if the components in the same thread of evnetloop.
+
+ private:
+
+  // to wake up EventLoop when it block on EventLoop::loop()
+  // specifically, use eventfd(2) trigger readable event to wake up
+  void WakeUp();
+  void HandleWakeUp();
+
+  void DoPendingFunctors();                    // execute other thread task in IO thread, in EventLoop::loop()
+
+  void AbortInLoopThread();                    // Log ERROR and abort programm.
   bool IsInLoopThread();
 
-  private:
-  void AbortInLoopThread();
-
-  bool looping_;
-  std::thread::id thread_id_;
+  int wakeup_fd_;                              // fd:        wakeup_fd_ , to wakeup IO thread, when block on EventLoop::loop()->poll;
+  std::unique_ptr<Channel> wakeup_channel_;    // channel:   use this to register into epoller and handle read event.
+  std::unique_ptr<EPoller> poller_;            // poller:    IO Mulplexing
+  std::vector<Functor> pending_functors_;      // vector:    store the functors, which other threads want to excute in IO thread
+  std::vector<Channel *> active_channels_;     // channel:   IO Mulplexing return the active channels;
+  std::atomic<bool> looping_;                  // bool:      to record the looping state, for ~EventLoop()
+  std::atomic<bool> quit_;                     // bool:      to record loop() state, use this to judge if go to next loop;
+  std::mutex mutex_;                           // mutex:     for pending_functors_ , this may cause race-condition.
+  std::thread::id thread_id_;                  // thread_id: record the EventLoop thread, which make it one loop per thread
 };
 
 }
