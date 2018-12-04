a045df610c476f40bf998bbf5395991324bd7281
Evil-crow <Evilcrow486@gmail.com>
2018-12-04 23:24:27 +0800

    Actually use EPoller::*Channel()

--- v2.0/reactor/event_loop.h
+++ v2.0/reactor/event_loop.h
@@ -17,41 +17,44 @@ class Channel;
 class EPoller;
 class EventLoop {
  public:
-  // All Functor or Callback, use this std::function format.
+  // All Functor or Callback, use this std::function<void ()> format.
   // You can use std::bind, lambda expression ( I suggest you use lambda expression !)
   using Functor = std::function<void ()>;
 
   EventLoop();
   ~EventLoop();
 
-  void Loop();                                 // The most important interface of EventLoop, to loop !
-  void RunInLoop(Functor cb);                  // One of the other important interface, allow other thread move their task into this IO thread
+  void Loop();                                         // The most important interface of EventLoop, to loop !
+  void Quit();                                         // if want to quit loop, use this interface(you may not need this indeed
+  void AddChannel(Channel *channel);                   // Actually use EPoller::AddChannel()
+  void RemoveChannel(Channel *channel);                // Actually use EPoller::RemoveChannel()
 
-  void Quit();                                 // if want to quit loop, use this interface(you may not need this indeed
-
-  bool AssertInLoopThread();                   // judge if the components in the same thread of evnetloop.
+  bool AssertInLoopThread();                           // judge if the components in the same thread of evnetloop.
+  //void RunInLoop(Functor &cb);                         // One of the other important interface, allow other thread move their task into this IO thread
 
  private:
-
   // to wake up EventLoop when it block on EventLoop::loop()
   // specifically, use eventfd(2) trigger readable event to wake up
   void WakeUp();
   void HandleWakeUp();
+  //void QueueInLoop(Functor &cb);
+  //void DoPendingFunctors();                                    // execute other thread task in IO thread, in EventLoop::loop()
 
-  void DoPendingFunctors();                    // execute other thread task in IO thread, in EventLoop::loop()
-
-  void AbortInLoopThread();                    // Log ERROR and abort programm.
+  void AbortInLoopThread();                                    // Log ERROR and abort programm.
   bool IsInLoopThread();
 
-  int wakeup_fd_;                              // fd:        wakeup_fd_ , to wakeup IO thread, when block on EventLoop::loop()->poll;
-  std::unique_ptr<Channel> wakeup_channel_;    // channel:   use this to register into epoller and handle read event.
-  std::unique_ptr<EPoller> poller_;            // poller:    IO Mulplexing
-  std::vector<Functor> pending_functors_;      // vector:    store the functors, which other threads want to excute in IO thread
-  std::vector<Channel *> active_channels_;     // channel:   IO Mulplexing return the active channels;
-  std::atomic<bool> looping_;                  // bool:      to record the looping state, for ~EventLoop()
-  std::atomic<bool> quit_;                     // bool:      to record loop() state, use this to judge if go to next loop;
-  std::mutex mutex_;                           // mutex:     for pending_functors_ , this may cause race-condition.
-  std::thread::id thread_id_;                  // thread_id: record the EventLoop thread, which make it one loop per thread
+  int wakeup_fd_;                                              // fd:        wakeup_fd_ , to wakeup IO thread, when block on EventLoop::loop()->poll;
+  std::unique_ptr<EPoller> epoller_;                           // epoller:    IO Mulplexing
+  std::shared_ptr<Channel> wakeup_channel_;                    // channel:   use this to register into epoller and handle read event.
+  std::vector<std::shared_ptr<Channel>> active_channels_;      // channel:   IO Mulplexing return the active channels;
+  std::atomic<bool> looping_;                                  // bool:      to record the looping state, for ~EventLoop()
+  std::atomic<bool> quit_;                                     // bool:      to record loop() state, use this to judge if go to next loop;
+  std::atomic<bool> eventing_handling_;                        // bool:      to record event handling state
+  std::mutex mutex_;                                           // mutex:     for pending_functors_ , this may cause race-condition.
+  std::thread::id thread_id_;                                  // thread_id: record the EventLoop thread, which make it one loop per thread
+
+  //std::atomic<bool> calling_pending_functor_;                  // bool:      to record the doing state
+  //std::vector<Functor> pending_functors_;                      // vector:    store the functors, which other threads want to excute in IO thread
 };
 
 }
