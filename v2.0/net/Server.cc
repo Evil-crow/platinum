eec767ba1958f52d092b4732fd6f0a5d04607224
Evil-crow <Evilcrow486@gmail.com>
2018-12-11 22:49:40 +0800

    delete it:

--- v2.0/net/Server.cc
+++
@@ -1,5 +0,0 @@
-//
-// Created by Crow on 11/25/18.
-//
-
-#include "Server.h"
--- v2.0/net/Server.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Crow on 11/25/18.
-//
-
-#ifndef PLATINUM_SERVER_H
-#define PLATINUM_SERVER_H
-
-#endif //PLATINUM_SERVER_H
