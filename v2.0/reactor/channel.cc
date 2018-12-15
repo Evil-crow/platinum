3dfe183f8705701766799b1fc78bf605ebf3d0c2
Evil-crow <Evilcrow486@gmail.com>
2018-12-15 23:43:23 +0800

    Use EPOLLRDHUP to replace EPOLLIN | EPOLLHUP

--- v2.0/reactor/channel.cc
+++ v2.0/reactor/channel.cc
@@ -5,6 +5,9 @@
 #include "reactor/channel.h"
 
 #include <sys/epoll.h>
+#include <cassert>
+
+#include "reactor/event_loop.h"
 
 using namespace platinum;
 
@@ -20,8 +23,9 @@ Channel::Channel(platinum::EventLoop *loop, int fd)
 
 Channel::~Channel()
 {
-  ;
+  assert(!event_handling_.load());                  // To make sure we won't destruct Channel when event handling;
 }
+
 
 // We only distinguish readable and the opposite hang up
 // the normal close and readable all will trigger EPOLLIN
@@ -29,19 +33,27 @@ Channel::~Channel()
 void Channel::HandleEvent()
 {
   event_handling_.store(true);
-  if (revents_ & (EPOLLHUP | EPOLLIN)) {
+
+  // handle hangup event
+  if (revents_ & EPOLLRDHUP) {                  // EPOLLRDHUP => EPOLLIN | EPOLLHUP
     if (close_callback_)
       close_callback_();
   }
+
+  // handle readable event, e.g. OnMessage
   if (revents_ & EPOLLIN) {
     if (read_callback_)
       read_callback_();
   }
+
+  // handle writeable event, e.g. WriteFd
   if (revents_ & EPOLLOUT) {
     if (write_callback_)
       write_callback_();
   }
   event_handling_.store(false);
+  std::cout << "Channel::HandleEvent() Ends" << '\n';
+  std::cout << "channel: " << fd_ << '\n';
 }
 
 void Channel::SetEvents(unsigned int events) { revents_ = events; }
@@ -51,6 +63,8 @@ void Channel::EnableReading()       { events_ |= EPOLLIN; }
 void Channel::EnableWriteing()      { events_ |= EPOLLOUT; }
 
 void Channel::EnableET()            { events_ |= EPOLLET; }
+
+void Channel::EnableHangUp()        { events_ |= EPOLLRDHUP; }
 
 void Channel::DisableWriting()      { events_ &= ~EPOLLOUT; }
 
