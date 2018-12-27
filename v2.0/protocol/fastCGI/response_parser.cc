43f640d373cce497311e8cbf002de4245fa8e2f9
Evil-crow <Evilcrow486@gmail.com>
2018-12-27 09:55:38 +0800

    modify the move operation to make parser correctly

--- v2.0/protocol/fastCGI/response_parser.cc
+++ v2.0/protocol/fastCGI/response_parser.cc
@@ -35,6 +35,7 @@ fcgi::ResponseParser::ResponseParser()
 
 bool ResponseParser::feed(ResponseParser::const_iter iter, int length)
 {
+  transform_data_.clear();
   // To ensure the last parsing result is complete
   if (transform_len_) {
     auto len = transform_len_ > length ? length : transform_len_;
@@ -67,14 +68,16 @@ bool ResponseParser::feed(ResponseParser::const_iter iter, int length)
       return false;
     }
 
-    Header header(iter);                                                          // Construct a header
+    Header header(iter);                                                           // Construct a header
+    iter += sizeof(Header);
+    length -= sizeof(Header);
     request_id_ = header.request_id();
     auto ct_len = header.content_length();
     auto pd_len = header.padding_length();
     switch (header.type()) {
       case Type::FCGI_STDOUT: ParserStdout(iter, length, ct_len, pd_len); break;
       case Type::FCGI_STDERR: ParserStderr(iter, length, ct_len, pd_len); break;
-      case Type::FCGI_END_REQUEST: ParserEndRequest(iter, length); break;
+      case Type::FCGI_END_REQUEST: ParserEndRequest(iter); break;
       default: break;
     }
   }
@@ -84,6 +87,7 @@ void ResponseParser::ParserStdout(const_iter &iter, int &length, int ct_len, int
 {
   if (ct_len == 0 && pd_len == 0)
     return ;
+
   auto len1 = ct_len > length ? length : ct_len;                   // judge if we have enough space to deal with
 
   std::string str(iter, iter + len1);
@@ -92,9 +96,9 @@ void ResponseParser::ParserStdout(const_iter &iter, int &length, int ct_len, int
     if ((pos = str.find("\r\n\r\n")) != std::string::npos) {
       name_value_data_.insert(name_value_data_.cend(), iter, iter + pos);
       iter += pos + 4;
-      length -= pos;
-      ct_len -= pos;
-      len1 -= pos;
+      length -= pos + 4;
+      ct_len -= pos + 4;
+      len1 -= pos + 4;
     } else {
       state_ = State::FAULT;
       complete_ = true;
@@ -109,7 +113,7 @@ void ResponseParser::ParserStdout(const_iter &iter, int &length, int ct_len, int
   ct_len -= len1;
 
   if (length == 0) {
-    transform_len_ += ct_len + pd_len;
+    transform_len_ += ct_len;
     padding_len_ = pd_len;
     return ;
   }
@@ -129,6 +133,7 @@ void ResponseParser::ParserStderr(const_iter &iter, int &length, int ct_len, int
 {
   if (ct_len == 0 && pd_len == 0)
     return ;
+
   auto len1 = ct_len > length ? length : ct_len;                   // judge if we have enough space to deal with
 
   transform_data_.insert(transform_data_.cend(), iter, iter + len1);
@@ -137,7 +142,7 @@ void ResponseParser::ParserStderr(const_iter &iter, int &length, int ct_len, int
   ct_len -= len1;
 
   if (length == 0) {
-    transform_len_ += ct_len + pd_len;
+    transform_len_ += ct_len;
     padding_len_ = pd_len;
     return ;
   }
@@ -153,9 +158,10 @@ void ResponseParser::ParserStderr(const_iter &iter, int &length, int ct_len, int
   }
 }
 
-void ResponseParser::ParserEndRequest(const_iter &iter, int &length)
+void ResponseParser::ParserEndRequest(const_iter &iter)
 {
   iter -= sizeof(Header);
+
   EndRequestRocord end_request_record(iter);
 
   app_status_ = end_request_record.app_status();
