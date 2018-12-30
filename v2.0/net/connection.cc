88cf9708e4cf5c91873076672e5d8893fb6c988c
Evil-crow <Evilcrow486@gmail.com>
2018-12-30 22:38:54 +0800

    modify class name from TCPConnection to Connection

---
+++ v2.0/net/connection.cc
@@ -0,0 +1,144 @@
+//
+// Created by Crow on 12/11/18.
+//
+
+#include "connection.h"
+
+#include <iostream>
+#include <unistd.h>
+#include <sys/sendfile.h>
+
+#include "net/socket.h"
+#include "reactor/event_loop.h"
+#include "reactor/channel.h"
+#include "utility/logger.h"
+
+using namespace platinum;
+
+Connection::Connection(EventLoop *loop, int fd)
+    : loop_(loop),
+      socket_(std::make_unique<Socket>(fd)),           // use move constructor
+      channel_(std::make_unique<Channel>(loop, fd))
+{
+  ;
+}
+
+Connection::~Connection()
+{
+  ;
+}
+
+void Connection::ConnectionEstablished()
+{
+  channel_->EnableET();
+  channel_->EnableReading();
+  channel_->EnableHangUp();
+  channel_->EnableError();
+  channel_->SetReadCallback  ([this]()  { Connection::HandleRead(); });
+  channel_->SetWriteCallback ([this]()  { Connection::HandleWrite(); });
+  channel_->SetCloseCallback ([this]()  { Connection::HandleClose(); });
+  channel_->SetErrorCallback ([this]()  { Connection::HandleError(); });
+  loop_->AddChannel(channel_.get());
+}
+
+void Connection::ShutDownConnection()
+{
+  ShutdownInLoop();
+}
+
+void Connection::ShutdownInLoop()
+{
+  loop_->RunInLoop([this]() { socket::ShutdownWrite(socket_->fd()); });
+}
+
+void Connection::SendData(const char *data, size_t total) {
+  size_t completed_(0), remained_(total);
+  while (true) {
+    auto var = ::write(socket_->fd(), data, total);
+    if (var > 0) {
+      if (var == total) {
+        return ;
+      } else {
+        completed_ += var;
+        remained_ -= var;
+      }
+    } else {
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        channel_->EnableWriteing();
+        loop_->UpdateChannel(channel_.get());
+        write_queue_.TaskInQueue(socket_->fd(), data, completed_, remained_);
+      } else {
+        LOG(ERROR) << "Connection::SendData()";
+      }
+    }
+  }
+}
+
+void Connection::SendFile(int file_fd, size_t total)
+{
+  size_t completed_(0), remained_(total);
+  while (true) {
+    auto var = ::sendfile64(socket_->fd(), file_fd, nullptr, total);
+    if (var > 0) {
+      if (var == total) {
+        return ;
+      } else {
+        completed_ += var;
+        remained_ -= var;
+      }
+    } else {
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        channel_->EnableWriteing();
+        loop_->UpdateChannel(channel_.get());
+        write_queue_.TaskInQueue(socket_->fd(), file_fd, completed_, remained_);
+      } else {
+        LOG(ERROR) << "Connection::SendData()";
+      }
+    }
+  }
+}
+
+void Connection::HandleRead() {
+  read_buffer_.ReadFd(socket_->fd());
+  if (message_callback_(this, read_buffer_))
+    read_buffer_.Reprepare();
+}
+
+void Connection::HandleWrite()
+{
+  if (write_queue_.DoTask()) {
+    channel_->DisableWriting();
+    loop_->UpdateChannel(channel_.get());
+  }
+}
+
+void Connection::HandleClose()
+{
+  loop_->RunInLoop([this]() { close_callback_(socket_->fd()); });
+}
+
+void Connection::HandleError()
+{
+  loop_->RunInLoop([this]() { ErrorCallback(); });
+}
+
+void Connection::ErrorCallback()
+{
+  LOG(ERROR) << "Connection::ErrorCallback()";
+  HandleClose();
+}
+
+void Connection::SetConnectionCallback(const EventCallback &callback)
+{
+  connection_callback_ = callback;
+}
+
+void Connection::SetMessageCallback(const MessageCallback &callback)
+{
+  message_callback_ = callback;
+}
+
+void Connection::SetCloseCallback(const CloseCallback &callback)
+{
+  close_callback_ = callback;
+}
---
+++ v2.0/net/connection.h
@@ -0,0 +1,63 @@
+//
+// Created by Crow on 12/11/18.
+//
+
+#ifndef PLATINUM_TCP_CONNECTION_H
+#define PLATINUM_TCP_CONNECTION_H
+
+#include <memory>
+#include <functional>
+
+#include "net/ip_address.h"
+#include "utility/noncopyable.h"
+#include "utility/buffer/buffer.h"
+#include "utility/buffer/writequeue.h"
+
+namespace platinum {
+
+class Socket;
+class IPAddress;
+class EventLoop;
+class Channel;
+class Connection : public Noncopyable {
+ public:
+  using EventCallback = std::function<void ()>;
+  using MessageCallback = std::function<bool (Connection *, Buffer &)>;
+  using CloseCallback = std::function<void (int)>;
+
+  Connection(EventLoop *loop, int fd);
+  ~Connection() override;
+
+  // move TCPConnection callback to Channel
+  void ConnectionEstablished();
+  void ShutDownConnection();
+  void SendData(const char *data, size_t total);
+  void SendFile(int file_fd, size_t total);
+
+  // copy TCPServer callback -> TCPConnection, can't move, we'll create much TCPConnection by TCPServer::*callback
+  void SetConnectionCallback(const EventCallback &callback);
+  void SetMessageCallback(const MessageCallback &callback);
+  void SetCloseCallback(const CloseCallback &callback);
+
+ private:
+  void HandleRead();                                   // deal with readable event, doing by Channel::read_callback_
+  void HandleWrite();                                  // deal with writeable event, doing by Channel::write_callback_
+  void HandleError();                                  // deal with error, ...
+  void HandleClose();                                  // deal with hangup event, doing by Channel::close_callback_
+
+  void ErrorCallback();                                // private function to record the error situation
+  void ShutdownInLoop();                               // push task in Loop to shutdown the connection
+
+  EventLoop *loop_;
+  std::unique_ptr<Socket> socket_;
+  std::unique_ptr<Channel> channel_;
+  EventCallback connection_callback_;                  // register by user provide function -> OnConnection fucntion
+  MessageCallback message_callback_;                   // register by user provide function -> OnMessage Function
+  CloseCallback close_callback_;                       // register as TCPServer::RemoveConnection
+  Buffer read_buffer_;
+  WriteQueue write_queue_;
+};
+
+}
+
+#endif //PLATINUM_TCP_CONNECTION_H
