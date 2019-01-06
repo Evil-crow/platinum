5b5ca4f874691d7c319697cc0930b6ab40ee6cd6
Evil-crow <Evilcrow486@gmail.com>
2019-01-06 22:35:27 +0800

    add parser to Connection. Each parser can be customed by inherit from platinum::Parser

--- v2.0/net/connection.h
+++ v2.0/net/connection.h
@@ -12,6 +12,7 @@
 #include <functional>
 
 #include "net/ip_address.h"
+#include "protocol/parser.hpp"
 #include "utility/noncopyable.h"
 #include "utility/buffer/buffer.h"
 #include "utility/buffer/writequeue.h"
@@ -25,10 +26,10 @@ class Channel;
 class Connection : public Noncopyable {
  public:
   using EventCallback = std::function<void ()>;
-  using MessageCallback = std::function<bool (Connection *, Buffer &)>;
+  using MessageCallback = std::function<long (Connection *, Buffer &, std::unique_ptr<Parser> &)>;
   using CloseCallback = std::function<void (int)>;
 
-  Connection(EventLoop *loop, int fd);
+  Connection(EventLoop *loop, int fd, ParserType type);
   ~Connection() override;
 
   // move TCPConnection callback to Channel
@@ -36,6 +37,7 @@ class Connection : public Noncopyable {
   void ShutDownConnection();
   void SendData(const char *data, size_t total);
   void SendFile(int file_fd, size_t total);
+  void ForceClose();
 
   // copy TCPServer callback -> TCPConnection, can't move, we'll create much TCPConnection by TCPServer::*callback
   void SetConnectionCallback(const EventCallback &callback);
@@ -51,10 +53,12 @@ class Connection : public Noncopyable {
 
   void ErrorCallback();                                // private function to record the error situation
   void ShutdownInLoop();                               // push task in Loop to shutdown the connection
+  void ForceCloseInLoop();
 
   EventLoop *loop_;
   std::unique_ptr<Socket> socket_;
   std::unique_ptr<Channel> channel_;
+  std::unique_ptr<Parser> parser_;
   EventCallback connection_callback_;                  // register by user provide function -> OnConnection fucntion
   MessageCallback message_callback_;                   // register by user provide function -> OnMessage Function
   EventCallback write_callback_;                       // register by Connector -> to check connection status, may be add timer
