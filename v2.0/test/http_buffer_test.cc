5b0c639ed0c3e13fe07a739df4bda2a7e4adfb66
Evil-crow <Evilcrow486@gmail.com>
2019-01-06 22:48:50 +0800

    not use httpxx, use platinum::http::RequestParser

--- v2.0/test/http_buffer_test.cc
+++ v2.0/test/http_buffer_test.cc
@@ -3,25 +3,31 @@
 //
 
 #include <iostream>
-#include <httpxx/Request.hpp>
 #include "reactor/event_loop.h"
 #include "net/connection.h"
 #include "net/tcp_server.h"
 #include "net/ip_address.h"
+#include "protocol/http/request_parser.h"
 #include "utility/buffer/buffer.h"
 
-bool OnMessage(platinum::Connection *connection, const platinum::Buffer &buf)
+long OnMessage(platinum::Connection *connection,
+               const platinum::Buffer &buf,
+               std::unique_ptr<platinum::Parser> &parser)
 {
   std::vector<unsigned char> vec(buf.BufferBegin(), buf.BufferEnd());
   std::string str(buf.BufferBegin(), buf.BufferEnd());
 //  std::cout << "str: " << str << std::endl;
-  http::Request request;
-  printf("Get message\n");
 
-  request.feed(vec.data(), vec.size());
-  if (!request.complete())
-    return false;
-  std::cout << "str: " << str << std::endl;
+  printf("Get message\n");
+//  std::cout << str << '\n';
+  auto parser_ = static_cast<platinum::http::RequestParser *>(parser.get());
+  auto len = parser_->feed(vec.cbegin(), vec.size());
+  if (parser_->Complete()) {
+    std::cout << parser_->status_line() << std::endl;
+    parser_->Reset();
+  }
+
+  return len;
 }
 
 int main()
