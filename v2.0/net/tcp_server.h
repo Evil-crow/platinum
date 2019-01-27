0a884947e34bc9ea30b949bcaaa8a3a1a6ee9eda
Evil-crow <Evilcrow486@gmail.com>
2019-01-27 21:20:49 +0800

    use Impl to replace TcpServer::GetCurrentServer() hack

--- v2.0/net/tcp_server.h
+++ v2.0/net/tcp_server.h
@@ -34,13 +34,16 @@ class TcpServer {
   void Start();                                                             // When set all thing, we can start the server;
   void SetConnectionCallback(const EventCallback &callback);                // I don't use it usually, It means resource initlization for users connection
   void SetMessageCallback(const MessageCallback &callback);                 // When message on, callback for user to perform logical business
-  void NewConnection(const Connector *connector);
-  void ForceClose(int fd);
-  auto NewConnector(const IPAddress &address, ParserType type) -> std::shared_ptr<Connector>;
-  auto NewConnector(const UnixAddress &address, ParserType type) -> std::shared_ptr<Connector>;
-
+  static void NewConnection(const Connector *connector);
+  static auto NewConnector(const IPAddress &address, ParserType type) -> std::shared_ptr<Connector>;
+  static auto NewConnector(const UnixAddress &address, ParserType type) -> std::shared_ptr<Connector>;
+  static void ForceClose(int fd);
 
  private:
+  auto NewConnectorImpl(const IPAddress &address, ParserType type) -> std::shared_ptr<Connector>;
+  auto NewConnectorImpl(const UnixAddress &address, ParserType type) -> std::shared_ptr<Connector>;
+  void NewConnectionImpl(const Connector *connector);
+  void ForceCloseImpl(int fd);
   void OnConnectionCallback(int fd, const IPAddress &address);             // private method for Acceptor::callback_; to construct Connection
   void EraseConnection(int fd);
 
